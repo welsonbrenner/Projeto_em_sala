c966fa05025631b3e777795397106d0df95c5534
Welson Brenner <welsonbrcosta@gmail.com>
2024-04-15 21:15:21 -0300

    first commit

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import { platformBrowserDynamic } from '@anglar/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
+  
